5b022be072c1ec2e54134e5229c805cc913b03d7
Scott Hoisington <hoyzington@gmail.com>
2019-11-21 02:24:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,19 +1,21 @@
 
-def win
-  
-  
+def user_input
+  gets.chomp
 end
 
 
 def number
-  
-  
+  rand(6) + 1
 end
 
 
 def win
-  
-  
+  puts "You guessed the correct number!"
+end
+
+
+def lose(num)
+  puts "Sorry! The computer guessed #{num}."
 end
 
 
